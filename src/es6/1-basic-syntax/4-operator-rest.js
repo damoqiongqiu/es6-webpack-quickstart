1871b5b8acd831c9b24ea0fb037fcc9d62ccd5dc
大漠穷秋 <damoqiongqiu@126.com>
2024-04-27 17:11:02 +0800

    update-example

--- src/es6/1-basic-syntax/4-operator-rest.js
+++ src/es6/1-basic-syntax/4-operator-rest.js
@@ -3,10 +3,10 @@
  * 1. 可变长参数
  * 2. 解构赋值
  * 注意：
- * rest和spread运算符的外观一模一样，都是3个点。
- * rest的中文含义是“剩余的”、“其余的”。
- * rest可以看成spread的反向操作，spread可以把数组“展开”，而rest可以把零散的元素“整合”成数组
- * rest操作符可以用来实现可变长参数，在Java里面有类似的实现，语法规则也类似，可变长参数必须是最后一个参数
+ * rest 和 spread 运算符的外观一模一样，都是3个点。
+ * rest 的中文含义是“剩余的”、“其余的”。
+ * rest 可以看成 spread 的反向操作，spread 可以把数组“展开”，而 rest 可以把零散的元素“整合”成数组
+ * rest 操作符可以用来实现可变长参数，在 Java 里面有类似的实现，语法规则也类似，可变长参数必须是最后一个参数
  * 可变长参数会被自动转换成一个标准的JS数组
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
  */
